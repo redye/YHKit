6bcf9420842366d78d57c63eb4e562fe9a6d7fc8
胡永红 <yonghong.hu@weimob.com>
2016-08-02 11:50:52 +0800

    rebuild

---
+++ KitDemo/CarouselViewController.h
@@ -0,0 +1,14 @@
+//
+//  CarouselViewController.h
+//  KitDemo
+//
+//  Created by Hu on 16/8/2.
+//  Copyright © 2016年 redye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface CarouselViewController : BaseViewController
+
+@end
--- KitDemo/ViewController.m
+++ KitDemo/ViewController.m
@@ -7,12 +7,13 @@
 //
 
 #import "ViewController.h"
-#import "YHCarouselView.h"
-#import "UIImageView+WebCache.h"
+#import "CarouselViewController.h"
 
-@interface ViewController ()<YHCarouselViewDelegate> {
-    NSArray *_imageUrls;
-}
+NSString * const kCell = @"Cell";
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *classes;
 @end
 
 @implementation ViewController
@@ -20,38 +21,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    YHCarouselView *carouselView = [[YHCarouselView alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, 200)];
-    carouselView.delegate = self;
-    [self.view addSubview:carouselView];
+    [self initData];
     
-    NSArray *imageNames = @[@"IMG_0010.JPG",
-                            @"IMG_0021.JPG",
-                            @"IMG_0023.JPG",
-                            @"IMG_0149.JPG",
-                            @"IMG_0151.JPG",
-                            @"IMG_0166.JPG"
-                            ];
-    _imageUrls = @[@"http://a.hiphotos.baidu.com/zhidao/pic/item/72f082025aafa40fa38bfc55a964034f79f019ec.jpg",
-                   @"http://photo.enterdesk.com/2011-2-16/enterdesk.com-1AA0C93EFFA51E6D7EFE1AE7B671951F.jpg",
-                   @"http://dl.bizhi.sogou.com/images/2012/09/30/44928.jpg",
-                   @"http://dl.bizhi.sogou.com/images/2012/03/08/96703.jpg",
-                   @"http://image.tianjimedia.com/uploadImages/2012/010/XC4Y39BYZT9A.jpg"
-                   ];
-    carouselView.imageCount = _imageUrls.count;
-//    [carouselView loadImageNames:imageNames];
+    [self setUI];
 }
 
-#pragma mark - YHCarouselViewDelegate
-- (void)carouselView:(YHCarouselView *)carouselView didShowAtIndex:(NSUInteger)index imageView:(UIImageView *)imageView {
-    NSString *url = [_imageUrls objectAtIndex:index];
-    [imageView sd_setImageWithURL:[NSURL URLWithString:url] placeholderImage:[UIImage imageNamed:@"pic_default"] completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-        NSLog(@"下载完成 %@", image);
-    }];
+- (void)initData {
+    self.classes = @[[CarouselViewController class]];
+}
+
+- (void)setUI {
+    self.title = @"Home";
+    
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kCell];
+    [self.view addSubview:_tableView];
+}
+
+#pragma mark - UITableViewDataSource, UITableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.classes.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCell forIndexPath:indexPath];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    NSString *text = NSStringFromClass(self.classes[indexPath.row]);
+    cell.textLabel.text = text;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    Class class = self.classes[indexPath.row];
+    UIViewController *viewController = [class new];
+    [self.navigationController pushViewController:viewController animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    
 }
 
 @end
